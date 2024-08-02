aee306d29e704e40a1fc8afa7b0253f7607679c1
Manato Miura <mmiura2351@gmail.com>
2024-08-02 13:42:02 +0000

    pingコマンドでpingを返すようにした

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -1,7 +1,10 @@
+import time
 import discord
 
 async def ping_command(interaction: discord.Interaction) -> None:
     """
-    Responds with Pong
+    Responds ping
     """
-    await interaction.response.send_message("Pong!")
+    send_time = interaction.created_at.timestamp()  # interaction.created_atをタイムスタンプに変換
+    delay = (time.time() - send_time) * 1000  # 遅延を計算
+    await interaction.response.send_message(f"Bot ping: {round(delay)}ms")
